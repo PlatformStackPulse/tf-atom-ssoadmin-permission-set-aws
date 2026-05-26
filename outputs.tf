4c07b6f61ac8de8ecd2c6c3223d11082f1e4fc0a
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-27 00:31:15 +0100

    feat: implement tf-atom-ssoadmin-permission-set-aws module
    * feat: implement tf-atom-ssoadmin-permission-set-aws module
    
    * fix: add full context variable declarations
    
    * fix: regenerate docs and resolve variable conflict

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "enabled" {
-  description = "Whether the module is enabled."
-  value       = local.enabled
+output "arn" {
+  description = "ARN of the permission set"
+  value       = try(aws_ssoadmin_permission_set.this[0].arn, null)
+}
+
+output "created_date" {
+  description = "Date the permission set was created"
+  value       = try(aws_ssoadmin_permission_set.this[0].created_date, null)
 }
