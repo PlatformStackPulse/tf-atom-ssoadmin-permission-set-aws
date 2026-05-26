3c2ddfe1e20b3b4a79918b80a5e3da663ee74db8
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:15:00 +0100

    feat: implement tf-atom-ssoadmin-permission-set-aws module

--- variables.tf
+++ variables.tf
@@ -1,7 +1,26 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "instance_arn" {
+  description = "ARN of the SSO instance"
+  type        = string
+  validation {
+    condition     = length(var.instance_arn) > 0
+    error_message = "instance_arn must not be empty."
+  }
+}
+
+variable "session_duration" {
+  description = "Session duration (ISO 8601, e.g., PT8H)"
+  type        = string
+  default     = "PT8H"
+}
+
+variable "relay_state" {
+  description = "Relay state URL"
+  type        = string
+  default     = null
+}
+
+variable "description" {
+  description = "Description of the permission set"
+  type        = string
+  default     = null
+}
